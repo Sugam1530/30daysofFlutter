12ae83b4ccaca5700409425b013f4c4013fa444f
sugamdas1530 <sugamdas1530@gmail.com>
2022-05-21 00:59:22 +0530

    day5 animation login

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_app/home_page.dart';
+import 'package:flutter_first_app/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/login_page.dart';
 
-void main(List<String> args) {
+import 'pages/home_page.dart';
+
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-
-
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  final int days = 30;
+  final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
-
-    final double days = 30;
-    final String name = "Sugam Looser";
-    bool isMale = true;
-    num temp = 30.5;
-
-    var day = "Tuesday";
-    const pi = 3.14;
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
-          child: Text("Welcome to $days days of Flutter by $name"),
+          child: Text("Welcome to $days days of flutter by $name"),
         ),
       ),
       drawer: Drawer(),
