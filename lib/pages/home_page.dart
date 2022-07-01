2a3ee02c09683ecdad434c2f3e6165f3c0450dac
Sugam Das <sugamdas1530@gmail.com>
2022-07-01 21:31:44 +0530

    local api  call

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_first_app/models/catalog.dart';
 import 'package:flutter_first_app/widgets/drawer.dart';
 
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+    loadData() async {
+     await Future.delayed(Duration(seconds: 2)); 
+     final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+     final decodedData = jsonDecode(catalogJson);
+     var productsData = decodedData["products"];
+     CatalogModel.items = List.from(productsData)
+                          .map<Item>((item) => Item.fromMap(item))
+                          .toList();
+      setState(() {});                    
+    }
+
+    void initState() {
+      super.initState();
+      loadData();
+    }
+
+  @override
   Widget build(BuildContext context) {
-
-    final double days = 30;
-    final String name = "Sugam";
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App",
@@ -22,11 +43,12 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)? ListView.builder(
           itemCount: CatalogModel.items.length,
           itemBuilder: (context, index){
             return ItemWidget(item: CatalogModel.items[index],);
           },
+        ):Center(child: CircularProgressIndicator(),
         ),
       ),
       drawer: MyDrawer(),
