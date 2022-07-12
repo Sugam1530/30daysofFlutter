76ca4a0186e1087452ff24203b6307e3f50230dd
Sugam Das <sugamdas1530@gmail.com>
2022-07-12 16:24:52 +0530

    last commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_first_app/core/store.dart';
+import 'package:flutter_first_app/models/cart.dart';
 import 'package:flutter_first_app/utils/routes.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -13,6 +15,7 @@ import 'package:flutter_first_app/widgets/themes.dart';
 
 import '../widgets/home_widgets/catalog_list.dart';
 import '../widgets/item_widget.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -23,10 +26,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  final url = "https://api.jsonbin.io/b/604dbddb683e7e079c4eefd3";
+
 
     loadData() async {
      await Future.delayed(const Duration(seconds: 2)); 
      final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+
+    // final response = await http.get(Uri.parse(url));
+    // final catalogJson = response.body;
+
      final decodedData = jsonDecode(catalogJson);
      var productsData = decodedData["products"];
      CatalogModel.items = List.from(productsData)
@@ -42,12 +51,17 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final _cart = (VxState.store as MyStore).cart;
     return Scaffold(
       backgroundColor: context.canvasColor,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
-      child: Icon(CupertinoIcons.cart, color: Colors.white,),
-      backgroundColor: context.theme.buttonColor,
+      floatingActionButton: VxBuilder(
+        mutations: {AddMutation, RemoveMutation},
+        builder:(ctx, _) => FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, MyRoutes.cartRoute),
+        child: Icon(CupertinoIcons.cart, color: Colors.white,),
+        backgroundColor: context.theme.buttonColor,
+        ).badge(color: Vx.red500,size: 22, count: _cart.items.length, textStyle: 
+        TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
       ),
       appBar: AppBar(
         title: const Text("Catalog App",
